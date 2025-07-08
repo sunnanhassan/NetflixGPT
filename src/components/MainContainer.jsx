3a0d70b1490d863cbce59dd0faa22a2272b45ffe
Sunnan Hassan <99334103+sunnanhassan@users.noreply.github.com>
2025-07-08 15:29:02 +0500

    temp update

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -1,7 +1,7 @@
 // MainContainer.js
 import { useSelector } from "react-redux";
 import VideoTitle from "./VideoTitle";
-import VideoBackground from "./VideoBackground";
+import VideoBackground from "./temp";
 
 const MainContainer = () => {
   const movies = useSelector((store) => store.movies?.nowPlaying);
